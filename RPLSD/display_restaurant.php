aff85135e8b975e027ea201328909b47a47b3570
initialds <initialds@github.com>
2014-11-05 14:26:35 +0700

    display restoran

---
+++ RPLSD/display_restaurant.php
@@ -0,0 +1,44 @@
+<html>
+	<head>
+	</head>
+
+	<?php 
+		include "connect.php"; 
+		$result = mysql_query("SELECT * FROM restaurant");
+	?>
+
+	<table>
+		<div>Daftar Restoran Sebandung</div>
+		<?php 
+				while ($row = mysql_fetch_assoc($result)){
+			?>
+		<tr>
+			<td>
+				<img src="<?php echo $row['gambar'] ?>" height="200" width = "200">
+			</td>
+			<td>
+				<div>Nama : <?php echo $row['nama'] ?></div>
+				<div>Deskripsi : <?php echo $row['deskripsi'] ?></div>
+				<div>Alamat : <?php echo $row['alamat'] ?></div>
+				<?php if(isset($promo['row'])) {?><div>Promo : <?php echo $row['promo'] ?></div> <?php } ?>
+
+				<?php
+					//SEARCHING RATING
+					$rate_result = mysql_query("SELECT AVG(rate) AS rate FROM `testimon` WHERE resto_id='". $row['id'] ."'");
+					$rate_row = mysql_fetch_assoc($rate_result);
+					if ($rate_row['rate']){
+						$rate = $rate_row['rate'];
+					}else{
+						$rate = 0;
+					}
+				?>
+				<div>Rating : <?php echo $rate; ?>/10</div>
+				<div><a href="index.php?id=<?php echo $row['id'] ?>">Rate now!</a> <a href="#">See testimony</a></div>
+				<div></div>
+ 			</td>
+		</tr>
+		<?php
+			}
+		?>
+	</table>
+</html>
--- RPLSD/index.php
+++ RPLSD/index.php
@@ -4,7 +4,7 @@
 <body>
 
 	<form action="rate.php" method="POST">
-		<input type="hidden" name="id" id="id" value="1"></input>
+		<input type="hidden" name="id" id="id" value="<?php echo $_GET["id"] ?>"></input>
 		<label> Testi </label><input type="text" name="testi" id="testi"></input>
 		<label> Rate </label><input type=number min="0" max="10" name="rate" id="rate"></input>
 		<input type="submit"></input>
